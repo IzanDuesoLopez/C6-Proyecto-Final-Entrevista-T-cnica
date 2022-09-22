c3a3836ebc0d57b6b861c2fea87aa750c868b483
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-09-22 09:06:47 +0200

    +

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -79,7 +79,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 	
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+	        registry.addMapping("/**").allowedOrigins("https://team3-c6-project.herokuapp.com/api/candidates/username/**").allowedMethods("*");
 	    }
 	 
 //	 @Override
