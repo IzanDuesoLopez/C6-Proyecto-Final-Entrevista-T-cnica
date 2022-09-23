ffcf0c2800102cc78492730b378b8cd4c22cba17
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-09-23 17:59:30 +0200

    trys

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -87,7 +87,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and().headers()
             // the headers you want here. This solved all my CORS problems! 
-            .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*")).and()
+            .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
+            .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE"))
+            .addHeaderWriter(new StaticHeadersWriter("Access-Control-Max-Age", "3600"))
+            .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+            .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization"))
+            .and()
 			.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
 	httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
