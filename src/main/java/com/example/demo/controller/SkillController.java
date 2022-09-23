83b66ee0df6e4aa941fe7ed180b16b71efac565c
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-09-23 19:27:21 +0200

    trys

--- src/main/java/com/example/demo/controller/SkillController.java
+++ src/main/java/com/example/demo/controller/SkillController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +21,22 @@ import com.example.demo.service.SkillServiceImpl;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class SkillController {
 
 	@Autowired
 	SkillServiceImpl skillServiceImpl;
+	
+	@GetMapping("/response-entity-builder-with-http-headers")
+	public ResponseEntity<String> usingResponseEntityBuilderAndHttpHeaders() {
+	    HttpHeaders responseHeaders = new HttpHeaders();
+	    responseHeaders.set("Baeldung-Example-Header", 
+	      "Value-ResponseEntityBuilderWithHttpHeaders");
+
+	    return ResponseEntity.ok()
+	      .headers(responseHeaders)
+	      .body("Response with header using ResponseEntity");
+	}
 	
 	@GetMapping("/skills")
 	public List<Skill> listarSkills(){
@@ -50,7 +63,7 @@ public class SkillController {
 	}
 
 	@PutMapping("/skills/{id}")
-	public Skill actualizarSkills(@PathVariable(name="id")int id,@RequestBody Skill skill) {
+	public ResponseEntity<Skill> actualizarSkills(@PathVariable(name="id")int id,@RequestBody Skill skill) {
 		
 		Skill skill_seleccionado= new Skill();
 		Skill skill_actualizado= new Skill();
@@ -61,7 +74,17 @@ public class SkillController {
 		
 		System.out.println("The updated Skill its: "+ skill_actualizado);
 		
-		return skill_actualizado;
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Access-Control-Allow-Origin", "*");
+		responseHeaders.set("Access-Control-Allow-Methods", "*");
+		responseHeaders.set("Access-Control-Max-Age", "3600");
+		responseHeaders.set("Access-Control-Allow-Credentials", "true");
+		responseHeaders.set("Access-Control-Allow-Headers", "*");
+		
+		
+		return ResponseEntity.ok()
+			      .headers(responseHeaders)
+			      .body(skill_actualizado);
 	}
 	
 	@DeleteMapping("/skills/{id}")
