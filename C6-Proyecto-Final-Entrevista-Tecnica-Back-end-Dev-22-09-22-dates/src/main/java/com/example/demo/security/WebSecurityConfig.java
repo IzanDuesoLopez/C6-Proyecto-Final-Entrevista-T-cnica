43135a72aa624a0caddc8daaef08a411eaa4d645
IzanDuesoLopez <izandueso@gmail.com>
2022-09-23 13:04:32 +0200

    test

--- C6-Proyecto-Final-Entrevista-Tecnica-Back-end-Dev-22-09-22-dates/src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ C6-Proyecto-Final-Entrevista-Tecnica-Back-end-Dev-22-09-22-dates/src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -97,7 +97,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 	 */
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/*");
     }
 
 }
