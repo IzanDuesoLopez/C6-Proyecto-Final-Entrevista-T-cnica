f7c243f748e80256ddf5f51e05e697c331f65768
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-09-23 11:06:39 +0200

    package dto documented

--- src/main/java/com/example/demo/dto/JwtResponse.java
+++ src/main/java/com/example/demo/dto/JwtResponse.java
@@ -7,10 +7,16 @@ public class JwtResponse implements Serializable {
 	private static final long serialVersionUID = -8091879091924046844L;
 	private final String jwttoken;
 
+	/**
+	 * @param jwttoken
+	 */
 	public JwtResponse(String jwttoken) {
 		this.jwttoken = jwttoken;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getToken() {
 		return this.jwttoken;
 	}
